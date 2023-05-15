1991538387e8e641d6c7b985c952cac80f124adc
scelsic2 <scelsic2@gmail.com>
2023-05-15 16:42:29 -0400

    Header.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-
+import { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
+import AddArtist from './components/AddArtist';
+import AddVendor from './components/AddVendor';
+import Artists from './components/Artists';
+import FAQs from './components/FAQs';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import News from './components/News';
+import Stages from './components/Stages';
+import Vendors from './components/Vendors';
+
 function App() {
+  
+   
+  
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>hello? is this thing on?</h1>
-      </header>
-    </div>
+
+    <>
+      <Header />
+    </>
+
   );
 }
 
