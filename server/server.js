cdb12281f2351ee6552b5427cf750b1efd923223
AuTangen <atangen2406@gmail.com>
2023-05-18 15:49:28 -0400

    yes

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: {secure: process.env.PORT ? true : false }
+    // cookie: {secure: process.env.PORT ? true : false }
   }))
 
 app.use('/api', api_routes)
